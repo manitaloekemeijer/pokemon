54ee955052c735d05e6ed0e60d6d4429999d2c81
manitaloekemeijer <maanietaa@hotmail.com>
2022-12-11 15:35:13 +0100

    Creation of card for JigglyPuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import PokemonCard from "./components/pokemonCard/PokemonCard";
+import axios from "axios";
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const [pokemon, setPokemon] = useState({});
+
+    async function fetchPokemon() {
+        try {
+            const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/JigglyPuff`);
+            console.log(result);
+            setPokemon(result);
+        } catch
+            (e) {
+            console.error(e);
+        }
+    }
+    fetchPokemon();
+
+    return (
+        <>
+            <h2>naam</h2>
+
+            <h3>abilities</h3>
+            <h3>weight</h3>
+            <h3>moves</h3>
+        </>
+
+    )
 }
 
 export default App;
